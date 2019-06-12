196d688aace7a3c2764ae569dfe19b398e1792b9
Navneeta <navneeta@ViewQwests-iMac-2.local>
2019-06-12 17:29:43 +0800

    Added my project

---
+++ routes/shubhanshu.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <!-- title that displays on tab -->
+    <title>
+        sample page
+    </title>
+    <!-- title close -->
+
+    <!-- all css -->
+    <style>
+
+    </style>
+    <!-- css close -->
+</head>
+<body>
+<!-- header of page contains menus and slider -->
+    <header>
+
+    </header>
+    <!-- header close -->
+
+<!-- between header and footer -->
+    <section>
+    </section>
+
+<!-- footer of page contain copyrights -->
+    <footer>
+    </footer>
+    <!-- footer close -->
+
+</body>
+</html>
