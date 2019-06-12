38efa86e612a463202019596406619055c29f6d1
Navneeta <navneeta@ViewQwests-iMac-2.local>
2019-06-12 17:32:43 +0800

    change welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome 
+                    Welcome to Reseller dashboard
                 </div>
 
                 <div class="links">
